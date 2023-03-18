9ecdfb5dd3b8122f9387a5da1b0947078bb132d6
Greg Russell <greg@metart.com>
2023-03-17 17:26:19 -0700

    removing node_modules

--- sql/initialize_database.sql
+++
@@ -1,6 +0,0 @@
-select 'Building map_quiz database...' as '';
-select 'Dropping existing database...' as '';
-select 'Creating database...' as '';
-create database if not exists map_quiz;
-
--- source create_tables.sql
